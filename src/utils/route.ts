37fa1f17f4df521fd0f0c217c98bad4e68ea7039
Frank Feng <frank@acho.io>
2022-03-26 13:57:45 +0800

    注释

--- src/utils/route.ts
+++ src/utils/route.ts
@@ -1,39 +1,54 @@
-import { diffPos, type Position } from ".";
+import type { Position } from ".";
 
+// 接受当前格子坐标，返回下一个格子坐标，游戏有自动判赢机制，到达终点自动停止
+// curPos 当前格子坐标
+// around 当前格子周围的格子通行情况，依次为上右下左[top, right, bottom, left]，0为可通信，1为墙壁，-1起点，-2终点
+// payload 全局存储挂载对象
+// uiHelpers uiHelpers.setCellStyle(`background-color: red`); 可设置当前格子的样式
 export function findNext(
   curPos: Position,
   around: (number | undefined)[],
   payload: any,
   uiHelpers: any
-) {
-  if (!payload.visited) {
-    payload.visited = {};
-  }
-
-  const { visited } = payload;
-  const key = getKey(curPos);
-  const movalePos = getAroundMovablePos(curPos, around);
-
-  if (!visited[key]) {
-    const prev = movalePos.find((pos) => !!(pos && visited[getKey(pos)]));
-    visited[key] = {
-      prev,
-      next: movalePos.filter((pos) => pos && getKey(pos) !== getKey(prev)),
-    };
-  }
-
-  const { prev, next } = visited[key];
-  let nextPos = prev;
-  if (next.length === 0) {
-    Reflect.deleteProperty(visited, key);
-  } else {
-    [nextPos] = next.splice(randomIntBetween(0, next.length - 1), 1);
-  }
-
-  setVisitedCountStlye(curPos, payload, uiHelpers);
-
-  return diffPos(nextPos, curPos);
+): Position {
+  return { x: curPos.x + 1, y: curPos.y + 1 };
 }
+
+// import { diffPos, type Position } from ".";
+// export function findNext(
+//   curPos: Position,
+//   around: (number | undefined)[],
+//   payload: any,
+//   uiHelpers: any
+// ) {
+//   if (!payload.visited) {
+//     payload.visited = {};
+//   }
+
+//   const { visited } = payload;
+//   const key = getKey(curPos);
+//   const movalePos = getAroundMovablePos(curPos, around);
+
+//   if (!visited[key]) {
+//     const prev = movalePos.find((pos) => !!(pos && visited[getKey(pos)]));
+//     visited[key] = {
+//       prev,
+//       next: movalePos.filter((pos) => pos && getKey(pos) !== getKey(prev)),
+//     };
+//   }
+
+//   const { prev, next } = visited[key];
+//   let nextPos = prev;
+//   if (next.length === 0) {
+//     Reflect.deleteProperty(visited, key);
+//   } else {
+//     [nextPos] = next.splice(randomIntBetween(0, next.length - 1), 1);
+//   }
+
+//   setVisitedCountStlye(curPos, payload, uiHelpers);
+
+//   return diffPos(nextPos, curPos);
+// }
 
 function getKey(pos: Position | undefined) {
   if (!pos) {
